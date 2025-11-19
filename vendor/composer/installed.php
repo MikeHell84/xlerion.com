60233789a7d826f2b08082e6de83ab6521f558e7
MikeHell84 <redxlerion@gmail.com>
2025-11-18 20:44:53 -0500

    Add vendor for server (temporary)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'xlerion/xlerion_cmr',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e138d29ab89b57e452d9e1da2d83c7f3bd8aac59',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'xlerion/xlerion_cmr' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e138d29ab89b57e452d9e1da2d83c7f3bd8aac59',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
